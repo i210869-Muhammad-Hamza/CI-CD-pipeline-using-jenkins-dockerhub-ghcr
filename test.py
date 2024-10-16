d8bcc6b896b8bac6674f5619e0d04d605de8a604
Muhammad Hamza <158145701+i210869-Muhammad-Hamza@users.noreply.github.com>
2024-10-16 15:16:48 +0500

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ class TestExample(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
     def test_addition(self):
-        self.assertEqual(1 + 3, 4)    
+        self.assertEqual(1 + 3, 4)
+    def test_addition(self):
+        self.assertEqual(1 + 5, 6) 
 
 if __name__ == '__main__':
     unittest.main()
